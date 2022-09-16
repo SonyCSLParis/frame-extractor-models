eb7dbffec631c6018acf0d50dee273c1bd9b81a2
remivantrijp <remi@csl.sony.fr>
2022-09-16 17:56:04 +0200

    Example file

---
+++ Italian/examples.lisp
@@ -0,0 +1,27 @@
+;;; Copyright (C) Sony Computer Science Laboratories Paris
+;;;               Authors: Martina Galletti martina.galletti@sony.com
+;;;                        Ines Blin ines.blin@sony.com
+;;;                        Remi van Trijp (www.remivantrijp.eu)
+;;;                        
+;;;
+;;;     This program is free software: you can redistribute it and/or modify
+;;;     it under the terms of the GNU General Public License as published by
+;;;     the Free Software Foundation, version 3 of the License.
+;;; 
+;;;     This program is distributed in the hope that it will be useful,
+;;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;     GNU General Public License for more details.
+;;; 
+;;;     You should have received a copy of the GNU General Public License
+;;;     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;; ----------------------------------------------------------------------------
+
+(in-package :ife)
+
+; (load-italian-frame-extractor)
+; (activate-monitor trace-fcg)
+
+(comprehend "un numero eccessivo di discendenti tale da comportare uno spezzettamento del patrimonio familiare" :cxn-inventory *italian-frame-extractor*)
+
+(comprehend "Le infezioni stanno diminuendo perché i vaccini sono efficienti." :cxn-inventory *italian-frame-extractor*)
