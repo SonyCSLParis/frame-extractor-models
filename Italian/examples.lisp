db6d013468931fb03dbc3ff24d4fe4f3f560edd3
remivantrijp <remi@csl.sony.fr>
2022-09-19 15:51:04 +0200

    - Examples with poiche - Some tricky cases not handled yet

--- Italian/examples.lisp
+++ Italian/examples.lisp
@@ -98,3 +98,61 @@
                          :cxn-inventory *italian-frame-extractor*
                          :frame-dev-monitor t)
 
+;;; ----------------------------------------------------------------------------
+;;; 3. poiché (because)
+;;; ----------------------------------------------------------------------------
+
+(extract-semantic-frames "nelle regioni colorate in bianco il paf non è calcolabile, poiché non si sono osservati decessi tra persone con livello di istruzione alto."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+(extract-semantic-frames "tra il 2008 e il 2018 vi è stato anche un calo di circa il 10% negli investimenti reali per singolo studente del terzo livello, poiché i budget pubblici non hanno tenuto il passo con il numero degli studenti."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+(extract-semantic-frames "il fatto non può bastare, poiché il problema degli investimenti internazionali si pone in primo luogo per il futuro."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+(extract-semantic-frames "ne risulterebbe una forte disuguaglianza dei redditi da lavoro, in parte fittizia, poiché essa diminuirebbe se si misurassero le disuguaglianze sul più lungo periodo, per esempio su dieci anni e non su uno solo , o anche sul totale della vita degli individui , ipotesi ideale per studiare davvero quelle disuguaglianze di opportunità e di destini di cui parlava vautrin, ma purtroppo difficile da realizzare per mancanza di dati."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+(extract-semantic-frames "qualsiasi contestazione della legittimità dei diritti di proprietà stabiliti in passato avrebbe rischiato di scoperchiare un vaso di pandora da cui la società non sarebbe mai uscita indenne, poiché nessuno avrebbe più saputo dove fermarsi."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+;; Two causation frames:
+(extract-semantic-frames "un obiettivo comunque non facile da realizzare, poiché presuppone un ampio coinvolgimento sociale e politico su questioni che certo riguardano tutti, ma la cui evidente complessità tecnica può portare ad affidarsi  ad altri ."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+(extract-semantic-frames "esclusione di per sé assurda, poiché porta a sottovalutare in maniera del tutto artificiale il livelli di prodotto interno e di reddito nazionale dei paesi che scelgono un sistema sanitario e scolastico pubblico anziché un sistema privato"
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+
+(extract-semantic-frames "nei grafici 6.1,6.4, gli interessi del debito pubblico, che non fanno parte del reddito nazionale e che remunerano un capitale che non fa parte del capitale nazionale (poiché il debito pubblico rientra nell'attivo per i detentori privati ma è un passivo per lo stato), non sono stati presi in considerazione."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+;; To check!
+(extract-semantic-frames "questo porta automaticamente a sottovalutare l'entità del deprezzamento del capitale naturale e a sopravvalutare la crescita reale, poiché questa è ottenuta in parte attingendo a riserve presenti da sempre e con effetti di inquinamento atmosferico e riscaldamento globale"
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
+;; TODO: error spacy
+(extract-semantic-frames "i suoi interlocutori erano meno pronti ad accettarlo poiché l'80% dell'aumento finiva direttamente nelle casse del tesoro pubblico."
+                         :phrase-based
+                         :cxn-inventory *italian-frame-extractor*
+                         :frame-dev-monitor t)
+
